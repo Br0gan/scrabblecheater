72ead9253ab360df63a7ea314b2d0ea51b69fad8
Corey Lee <slicklogic@gmail.com>
2014-07-03 10:50:38 -0400

    first commit

---
+++ scrabble.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python
+
+import argparse, re, sys
+
+f = open('sowpods.txt', 'r')
+text = f.read()
+data = text.lower().split()
+
+parser = argparse.ArgumentParser(description='Scabble Cheater!!!')
+parser.add_argument('letters', metavar="letters", type=str,
+                    help='Enter your current letters')
+args = parser.parse_args()
+
+userLetters = args.letters.lower()
+userList = list(userLetters)
+userList = sorted(userList)
+
+scores = {"a": 1, "c": 3, "b": 3, "e": 1, "d": 2, "g": 2,
+          "f": 4, "i": 1, "h": 4, "k": 5, "j": 8, "m": 3,
+          "l": 1, "o": 1, "n": 1, "q": 10, "p": 3, "s": 1,
+          "r": 1, "u": 1, "t": 1, "w": 4, "v": 4, "y": 4,
+          "x": 8, "z": 10}
+
+
+digits = re.compile('\d')
+letCheck = re.compile('\w')
+
+def containsDigits(d):
+    return(bool(digits.search(d)))
+
+if len(sys.argv) < 2 or containsDigits(userLetters) == True:
+    print("Please suplly only  letters as argument")
+    exit(0)
+
+def wordSearch(l):
+    for word in data:
+        wl = []
+        x = 0
+        if len(word) <=  len(l):
+            for letter in l:
+                if word.find(letter) !=-1:
+                    try:
+                        wl.remove(word.find(letter))
+                        x += x
+                    except ValueError:
+                        wl.insert(word.find(letter), letter)
+                        if len(wl) == len(word):
+                            newTest = ','.join(wl)
+                            newTest = letCheck.search(newTest)
+                            if newTest == word:
+                                print(word)
+                                result.append(word)
+                    else:
+                        wl.insert(word.find(letter), letter)
+                        if len(wl) + x == len(word):
+                            newTest = ','.join(wl)
+                            newTest = letCheck.search(newTest)
+                            if newTest != word:
+                                print(word)
+                                result.append(word)
+
+result = []
+wordSearch(userList)
+print(result)
